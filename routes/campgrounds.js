0c06a3aee23c07c3d868228dd635b7ba87f6a9d9
Frank Pitkat <fpitkat@gmail.com>
2023-04-02 15:04:58 -0400

    multer

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,11 +3,19 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router
 	.route('/')
 	.get(catchAsync(campgrounds.index))
-	.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampfround));
+	.post(
+		isLoggedIn,
+		upload.array('image'),
+		validateCampground,
+		catchAsync(campgrounds.createCampground)
+	);
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
