986438a9fc8728ce03adc41f1f5f4518a4874330
Frank Pitkat <fpitkat@gmail.com>
2023-04-03 14:18:06 -0400

    minor bug fix

--- seeds/index.js
+++ seeds/index.js
@@ -27,10 +27,19 @@ const seedDB = async () => {
 			author: '64247d57bc86a5459b3fd35f',
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
-			image: 'https://source.unsplash.com/collection/483251',
 			description:
 				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
 			price,
+			images: [
+				{
+					url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+					filename: 'YelpCamp/ahfnenvca4tha00h2ubt',
+				},
+				{
+					url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+					filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi',
+				},
+			],
 		});
 		await camp.save();
 	}
