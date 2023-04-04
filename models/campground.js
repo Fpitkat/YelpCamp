faf7fa81efd3d13edf77fae0bad34356d19b4d70
Frank Pitkat <fpitkat@gmail.com>
2023-04-04 17:15:01 -0400

    fished styling

--- models/campground.js
+++ models/campground.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+// https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
+
 const ImageSchema = new Schema({
 	url: String,
 	filename: String,
@@ -11,22 +13,43 @@ ImageSchema.virtual('thumbnail').get(function () {
 	return this.url.replace('/upload', '/upload/w_200');
 });
 
-const CampgroundSchema = new Schema({
-	title: String,
-	images: [ImageSchema],
-	price: Number,
-	description: String,
-	location: String,
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-	},
-	reviews: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Review',
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+	{
+		title: String,
+		images: [ImageSchema],
+		geometry: {
+			type: {
+				type: String,
+				enum: ['Point'],
+				required: true,
+			},
+			coordinates: {
+				type: [Number],
+				required: true,
+			},
 		},
-	],
+		price: Number,
+		description: String,
+		location: String,
+		author: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+		},
+		reviews: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Review',
+			},
+		],
+	},
+	opts
+);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+	return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>`;
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
--- seeds/index.js
+++ seeds/index.js
@@ -20,16 +20,21 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
 	await Campground.deleteMany({});
-	for (let i = 0; i < 50; i++) {
+	for (let i = 0; i < 500; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
 		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
+			//YOUR USER ID
 			author: '64247d57bc86a5459b3fd35f',
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
 			description:
 				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
 			price,
+			geometry: {
+				type: 'Point',
+				coordinates: [cities[random1000].longitude, cities[random1000].latitude],
+			},
 			images: [
 				{
 					url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
